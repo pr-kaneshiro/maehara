2ead4cb775562382e0958f6c6307872e4be2e107
ps-r-kaneshiro <r-kaneshiro@pdc.proto-g.co.jp>
2019-11-01 21:32:08 +0900

    初めての方へ　コーディング完了

--- js/pr_script.js
+++ js/pr_script.js
@@ -10,28 +10,25 @@
             page_id = $('main').attr('id');
             console.log(page_id);
         }
-    }
+    }   
     MAEHARA.PRESENT = {
         init: function () {
-            if (page_id === 'present') {
-                //body Class add
-                $('body').addClass('pr_mae-present-page');
-                //jaounal only
-            }
+            //body Class add
+            $('body').addClass('pr_mae-present-page');
             /*slider001*/
             $('.pr_mae-pre-sc-bsbox-slider').bxSlider({
-    		    auto: true,
+                auto: true,
                 infiniteLoop: true,
                 pager:false,
-    		    pause: 5000,
-    		    speed: 1500,
+                pause: 5000,
+                speed: 1500,
                 slideWidth: 200,
                 slideMargin: 20,
-    		    controls: false,
-    		    captions: true,
-    		    mode: 'horizontal',
+                controls: false,
+                captions: true,
+                mode: 'horizontal',
                 responsive: true,
-			});
+            });
             $('.slick-test').slick({
                 infinite: true,
                 dots:true,
@@ -54,19 +51,82 @@
             });
         }
     }
-    var win_w = $(window).width();
-    var slide_resize = (win_w / 100) * 53;
+    MAEHARA.BEGINER = {
+        init: function () {
+            $('body').addClass('pr_mae-beginer-page');
+            /*SP*/
+            if(window.matchMedia('screen and (max-width: 768px)').matches) {
+                console.log('SP!!!');
+                $('.pr_mae-beg-base-slide001').bxSlider({
+                    auto: true,
+                    infiniteLoop: true,
+                    pager:true,
+                    pause: 5000,
+                    speed: 1500,
+                    controls: true,
+                    captions: true,
+                    mode: 'horizontal',
+                    prevText: '',
+                    nextText: '',
+                    prevSelector: '.pr_mae-beg-arrow-left',
+                    nextSelector: '.pr_mae-beg-arrow-right',
+                    responsive: true,
+                });
+            }
+            /*PC*/
+            else {
+                console.log('PC!!!');
+                $('.pr_mae-beg-base-slide001').bxSlider({
+                    auto: true,
+                    infiniteLoop: true,
+                    pager:true,
+                    pause: 5000,
+                    speed: 1500,
+                    controls: true,
+                    captions: true,
+                    mode: 'horizontal',
+                    minSlides: 3,
+                    maxSlides: 3,       //画面に表示する最大要素数
+                    moveSlides: 3,      //一回のスクロールで動かす要素数
+                    slideWidth: slide_resize,    //一つの要素の幅
+                    prevText: '',
+                    nextText: '',
+                    prevSelector: '.pr_mae-beg-arrow-left',
+                    nextSelector: '.pr_mae-beg-arrow-right',
+                    responsive: true,
+                }); 
+            }
+        },
+        slide: function () {
+            // $('.pr_mae-beg-base-slide001').destroySlider();
+            $('.pr_mae-pre-sc-bsbox-slider').bxSlider({
+                slideWidth: slide_resize,
+            });
+        }
+    }
     $(document).ready(function () {
         win_w = $(window).width();
         slide_resize = (win_w / 100) * 53;
         MAEHARA.COMMON.init();
-        MAEHARA.PRESENT.init();
+        if (page_id === 'present') {
+            MAEHARA.PRESENT.init();
+        }
+        else if (page_id === 'beginer') {
+            slide_resize = $('.pr_mae-beg-slide-box').width() / 3;
+            MAEHARA.BEGINER.init();
+            sliderFlag = true;
+        }
     });
     $(window).on('resize', function () {
-        win_w = $(window).width();
         slide_resize = (win_w / 100) * 53;
         // console.log(win_w,slide_resize);
-        // MAEHARA.PRESENT.slide();
+        if (page_id === 'present') {
+            MAEHARA.PRESENT.slide();
+        }
+        else if (page_id === 'beginer') {
+            slide_resize = $('.pr_mae-beg-slide-box').width() / 3;
+            MAEHARA.BEGINER.slide();
+        }
     });
 })(jQuery);
 
