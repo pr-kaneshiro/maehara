ea681b86c71aa927917e12670c3efaffae537339
ps-r-kaneshiro <r-kaneshiro@pdc.proto-g.co.jp>
2019-11-05 21:00:35 +0900

    初めての方へ,傘の贈り物　CMSコーディング完了（実機未検証）

--- js/pr_script.js
+++ js/pr_script.js
@@ -1,7 +1,8 @@
 /***kaneshiro add***/
 
 /*present page*/
-(function ($) {
+jQuery.noConflict();
+jQuery(function ($) {
     var page_id = 'default';
     MAEHARA = {};
     //common
@@ -9,6 +10,38 @@
         init: function () {
             page_id = $('main').attr('id');
             console.log(page_id);
+            //smoothscroll
+            //SP
+            if (window.matchMedia('screen and (max-width: 768px)').matches) {
+                var headerHeight = $('.header-middle').outerHeight();
+            }
+            //PC
+            else {
+                var headerHeight = $('#header').outerHeight();
+            }
+            var urlHash = location.hash; //URLハッシュを取得
+            var animeSpeed = 500; //スクロールのアニメーションスピード
+            if (urlHash) {
+              $('body,html').stop().scrollTop(0);
+              setTimeout(function () {
+                var target = $(urlHash);
+                var position = target.offset().top - headerHeight; //ヘッダー固定の場合：headerHeight、そうでない場合：0
+                $('body,html').stop().animate({
+                  scrollTop: position
+                }, animeSpeed);
+              }, 100);
+            }
+            $('a[href^="#"]').on({
+              'click': function () {
+                console.log('OK!!!!!');
+                var href = $(this).attr("href");
+                var target = $(href);
+                var position = target.offset().top - headerHeight; //ヘッダー固定の場合：headerHeight、そうでない場合：0
+                $('body,html').stop().animate({
+                  scrollTop: position
+                }, animeSpeed);
+              }
+            });
         }
     }   
     MAEHARA.PRESENT = {
@@ -29,24 +62,24 @@
                 mode: 'horizontal',
                 responsive: true,
             });
-            $('.slick-test').slick({
-                autoplay: false,
-                speed: 1000,
-                variableWidth: true,
-                centerMode: false,
-                centerPadding: "0",
-                infinite: false,
-                dots:true,
-                arrows: false,
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                autoplaySpeed: 3000,
-                pauseOnFocus: false,
-                pauseOnHover:false,
-                // adaptiveHeight:true,
-                prevArrow: '<span class="pr_atr-top-slide-arrow-prev js-pr_height-adj"></span>',
-                nextArrow: '<span class="pr_atr-top-slide-arrow-next js-pr_height-adj"></span>'
-            });
+            // $('.slick-test').slick({
+            //     autoplay: false,
+            //     speed: 1000,
+            //     variableWidth: true,
+            //     centerMode: false,
+            //     centerPadding: "0",
+            //     infinite: false,
+            //     dots:true,
+            //     arrows: false,
+            //     slidesToShow: 1,
+            //     slidesToScroll: 1,
+            //     autoplaySpeed: 3000,
+            //     pauseOnFocus: false,
+            //     pauseOnHover:false,
+            //     // adaptiveHeight:true,
+            //     prevArrow: '<span class="pr_atr-top-slide-arrow-prev js-pr_height-adj"></span>',
+            //     nextArrow: '<span class="pr_atr-top-slide-arrow-next js-pr_height-adj"></span>'
+            // });
         },
         slide: function () {
             // $('.pr_mae-pre-sc-bsbox-slider').bxSlider({
@@ -90,9 +123,9 @@
                     captions: true,
                     mode: 'horizontal',
                     minSlides: 3,
-                    maxSlides: 3,       //画面に表示する最大要素数
-                    moveSlides: 3,      //一回のスクロールで動かす要素数
-                    slideWidth: slide_resize,    //一つの要素の幅
+                    maxSlides: 3,
+                    moveSlides: 3,
+                    slideWidth: slide_resize,
                     prevText: '',
                     nextText: '',
                     prevSelector: '.pr_mae-beg-arrow-left',
@@ -102,13 +135,27 @@
             }
         },
         slide: function () {
-            // $('.pr_mae-beg-base-slide001').destroySlider();
             $('.pr_mae-pre-sc-bsbox-slider').bxSlider({
                 slideWidth: slide_resize,
             });
         }
     }
-    $(document).ready(function () {
+    var win_w = $(window).width();
+    var slide_resize = 0;
+    // $(document).ready(function () {
+    //     win_w = $(window).width();
+    //     slide_resize = (win_w / 100) * 53;
+    //     MAEHARA.COMMON.init();
+    //     if (page_id === 'present') {
+    //         MAEHARA.PRESENT.init();
+    //     }
+    //     else if (page_id === 'beginer') {
+    //         slide_resize = $('.pr_mae-beg-slide-box').width() / 3;
+    //         MAEHARA.BEGINER.init();
+    //         sliderFlag = true;
+    //     }
+    // });
+    $(window).on('load', function () {
         win_w = $(window).width();
         slide_resize = (win_w / 100) * 53;
         MAEHARA.COMMON.init();
@@ -118,12 +165,10 @@
         else if (page_id === 'beginer') {
             slide_resize = $('.pr_mae-beg-slide-box').width() / 3;
             MAEHARA.BEGINER.init();
-            sliderFlag = true;
         }
     });
     $(window).on('resize', function () {
         slide_resize = (win_w / 100) * 53;
-        // console.log(win_w,slide_resize);
         if (page_id === 'present') {
             MAEHARA.PRESENT.slide();
         }
@@ -132,6 +177,6 @@
             MAEHARA.BEGINER.slide();
         }
     });
-})(jQuery);
+});
 
 /***end***/
