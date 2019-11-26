18b69653ecb8390b0eba9f9d58cd24ed6d6e31fe
PDC\ky-k-yogi <ke-yogi@pdc.proto-g.co.jp>
2019-11-26 10:12:06 +0900

    FRBXPRT-883 修理について

--- js/pr_script002.js
+++ js/pr_script002.js
@@ -3,11 +3,11 @@
 /*present page*/
 jQuery.noConflict();
 jQuery(function ($) {
-    var page_id = 'default';
+    page_id = 'default';
     page_id = $('main').attr('id');
     var MAEHARA = {};
 
-    MAEHARA.ATHAND = {}
+    MAEHARA.ATHAND = {};
     MAEHARA.ATHAND.init = function () {
         $('.pr_mae-athand-book-listTtl').on('click', function () {
             $(this).toggleClass('pr_js-active');
@@ -15,11 +15,15 @@ jQuery(function ($) {
         });
     }
 
+    MAEHARA.REPAIR = {};
+    MAEHARA.REPAIR.init = function () {
+    }
+
     if (page_id === 'hand-picturebook') {
         MAEHARA.ATHAND.init();
     }
     if (page_id === 'repair') {
-
+        MAEHARA.REPAIR.init();
     }
 });
 
